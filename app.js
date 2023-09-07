b57fd277796701c4ebbbfd4a3664a892612eb117
Jeremie Roberrini <140409352+Jeremie-R@users.noreply.github.com>
2023-09-07 11:42:19 +0200

    first function link to call the text based game

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 
 // collect user input for rock paper scissor
+document.getElementById('textGameButton').addEventListener('click', textGame);
+
+function textGame() {
+    console.log("button clicked")
+}
+
 // get a random rock paper scissor for computer
 // define who wins
 // display the winner
